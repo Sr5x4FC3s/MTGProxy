6bdc1f2e16a76e12b5ab06d354c4f86cb3b5431e
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-10-01 16:00:33 -0700

    added basic textbox and scaffolding to the page structure / also included a mock list with ul and li tags

--- src/containers/deckBuilder.jsx
+++ src/containers/deckBuilder.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 
 export default class DeckBuilder extends Component {
+  
+  handleChange() {
+    console.log('hello');
+  }
+
   render() {
     return (
       <div id="deckBuilder">
         This is the deck builder page.
+        <div>
+          <form id="deck-builder-form">
+            <label>
+              Input Card Names (Separate them by commas if you wish to enter more than one card at a time)
+            </label>
+            <br></br><br></br>
+            <input type="submit" value="Submit Cards"/>
+          </form>
+          <textarea 
+            id="deck-entry"
+            form="deck-builder-form">
+            Enter Deck Information
+          </textarea>
+        </div>
+        <br></br><br></br><br></br>
+        <div>
+          <ul>
+            <li>card1</li>
+            <li>card2</li>
+            <li>card3</li>
+            <li>card4</li>
+          </ul>
+        </div>
       </div>
     );
   }
