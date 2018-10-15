d7f77fe302a5ebfe4972fa356e08c43282b547ba
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-10-15 15:47:26 -0700

    added express router

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser      = require('body-parser');
 const path            = require('path');
 const cors            = require('cors');
 const connectMongoose = require('../database/mongoose/connect.js');
-const SHF             = require('../helperFunctions/serverHelperFunctions/helper.js');
+const routes          = require('./routes.js');
 
 const PORT = 3003;
 
@@ -19,72 +19,6 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, '../dist')));
 
-app.get('/cardretrieval/:cardarray', (req, res) => {
-  let data = SHF.convertString(req.params.cardarray); //['String']
-
-  let promise = new Promise((resolve, reject) => {
-    let queried = SHF.queryDatabase(data);
-    resolve(queried);
-  }).then((result) => {
-    return new Promise((resolve, reject) => {
-      let collectedData = result;
-      resolve(collectedData);
-    })
-  }).then((result) => {
-    console.log('collected data', result);
-    res.status(200).send(result);
-  })
-});
-
-app.get(`/retrievedeck/:deck`, (req, res) => {
-  let data = req.params.deck;
-  let data2Array = [data];
-
-  let promise = new Promise((resolve, reject) => {
-    let query = SHF.retrieveOneDeck(data2Array);
-    resolve(query);
-  }).then(result => {
-    return new Promise((resolve, reject) => {
-      let deck = result;
-      resolve(deck);
-    }).then(result => {
-      res.status(200).send(result);
-    })
-  })
-})
-
-app.get(`/retrieveAllDecks`, (req, res) => {
-  let promise = new Promise((resolve, reject) => {
-    let query = SHF.retrieveAllDecks();
-    resolve(query);
-  }).then((result) => {
-    return new Promise ((resolve, reject) => {
-      let collectedDecks = result;
-      resolve(collectedDecks);
-    }).then((result) => {
-      //send the res back here
-      res.status(200).send(result);
-    })
-  });
-})
-
-app.post('/cardsubmission', (req, res) => {
-  let data = SHF.convertString(req.body.deckList); // ['String']
-  let promise = SHF.getRequest(data);
-  res.status(200).send('Posted Data');
-})
-
-app.post('/decksubmission', (req, res) => {
-  let deckList = req.body.currentDeck; //['String']
-  let deckName = req.body.deckName; //'String' 
-  let deckType = req.body.deckType; //'String'
-  let promise = SHF.submitDeck(deckName, deckList, deckType);
-
-  res.status(200).send('Deck is posted');
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve('dist', 'index.html'));
-});
+app.use('/', routes);
 
 app.listen(PORT, () => { console.log(`server is connected to port ${PORT}`)});
---
+++ server/routes.js
@@ -0,0 +1,76 @@
+const express    = require('express');
+const routes     = express.Router();
+const bodyParser = require('body-parser');
+const path       = require('path');
+const SHF        = require('../helperFunctions/serverHelperFunctions/helper.js');
+
+
+routes.get('/cardretrieval/:cardarray', (req, res) => {
+  let data = SHF.convertString(req.params.cardarray); //['String']
+
+  let promise = new Promise((resolve, reject) => {
+    let queried = SHF.queryDatabase(data);
+    resolve(queried);
+  }).then((result) => {
+    return new Promise((resolve, reject) => {
+      let collectedData = result;
+      resolve(collectedData);
+    })
+  }).then((result) => {
+    console.log('collected data', result);
+    res.status(200).send(result);
+  })
+});
+
+routes.get(`/retrievedeck/:deck`, (req, res) => {
+  let data = req.params.deck;
+  let data2Array = [data];
+
+  let promise = new Promise((resolve, reject) => {
+    let query = SHF.retrieveOneDeck(data2Array);
+    resolve(query);
+  }).then(result => {
+    return new Promise((resolve, reject) => {
+      let deck = result;
+      resolve(deck);
+    }).then(result => {
+      res.status(200).send(result);
+    })
+  })
+})
+
+routes.get(`/retrieveAllDecks`, (req, res) => {
+  let promise = new Promise((resolve, reject) => {
+    let query = SHF.retrieveAllDecks();
+    resolve(query);
+  }).then((result) => {
+    return new Promise ((resolve, reject) => {
+      let collectedDecks = result;
+      resolve(collectedDecks);
+    }).then((result) => {
+      //send the res back here
+      res.status(200).send(result);
+    })
+  });
+})
+
+routes.post('/cardsubmission', (req, res) => {
+  let data = SHF.convertString(req.body.deckList); // ['String']
+  let promise = SHF.getRequest(data);
+  res.status(200).send('Posted Data');
+})
+
+routes.post('/decksubmission', (req, res) => {
+  let deckList = req.body.currentDeck; //['String']
+  let deckName = req.body.deckName; //'String' 
+  let deckType = req.body.deckType; //'String'
+  let promise = SHF.submitDeck(deckName, deckList, deckType);
+
+  res.status(200).send('Deck is posted');
+})
+
+routes.get('*', (req, res) => {
+  res.sendFile(path.resolve('dist', 'index.html'));
+});
+
+module.exports = routes;
