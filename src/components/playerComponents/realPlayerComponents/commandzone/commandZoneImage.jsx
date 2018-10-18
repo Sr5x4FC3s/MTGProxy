2b26111fd27f159b88a24c81e61e966238e24024
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-10-18 14:29:17 -0700

    conditional render based on whether the commander is located -- command zone, hand or battlefield

--- src/components/playerComponents/realPlayerComponents/commandzone/commandZoneImage.jsx
+++ src/components/playerComponents/realPlayerComponents/commandzone/commandZoneImage.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 
 const CommandZoneImage = (props) => {
   //conditonal render based on whether image value is filled or not
-  return (
-    <div id="command-zone-image">
-      {props.props.state.commanderImageUrl !== null ? (
-        <img id="commander-image" src={props.props.state.commanderImageUrl}></img>
-      ) : (
-        null
-      )}
-    </div>
-  )
+
+  //render if commmander is in command zone
+  if (props.props.state.CZinCommandZone) {
+    return (
+      <div id="command-zone-image">
+        {props.props.state.commanderImageUrl !== null ? (
+          <img id="commander-image" src={props.props.state.commanderImageUrl}></img>
+        ) : (
+          null
+        )}
+      </div>
+    )
+  }
+  //render if in play
+  if (props.props.state.CZinPlay) {
+    return (
+      <div id="commander-in-play">Commander Is Currently In Play</div>
+    )
+  }
+  //render if in hand
+  if (props.props.state.CZinHand) {
+    return (
+      <div id="commander-in-hand">Commander Is Currently In Hand</div>
+    )
+  }
 };
 
 export default CommandZoneImage;
